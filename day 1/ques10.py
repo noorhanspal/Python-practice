58fe836798482b6726399c2fc58215b490713541
noorhanspal <deepnoor977@gmail.com>
2026-08-06 22:43:45 +0530

    Added question 10

---
+++ day 1/ques10.py
@@ -0,0 +1,6 @@
+# check whether a number is multiple of 10 or not
+num = int(input("enter a number : "))
+if (num%10==0):
+  print("number is multiple of 10")
+else:
+  print("number is not multiple of 10")
